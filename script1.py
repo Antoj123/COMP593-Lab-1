49b0a76928c4a20c9f31b9a95fa6d86de3d28178
Antoj123 <antoj@flemingcollege.ca>
2024-01-16 08:59:11 -0500

    created script files
    Added code that prints lines from Zen of Python

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Simple is better than complex.")
+print("Special cases aren't special enough to break the rules.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("Flat is better than nested.")
+print("Now is better than never.")
