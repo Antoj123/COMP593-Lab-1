63a779c1b7652d98a658c9d2dcb24bb888917155
Antoj123 <antoj@flemingcollege.ca>
2024-01-16 09:08:30 -0500

    Revert "modified both script files"
    This reverts commit 88254f9c9e508ef8e858d8959e7c39e35fe9ebfa.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 # Print lines from zen of Python
 print("Simple is better than complex.")
+print("Special cases aren't special enough to break the rules.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# Print lines from zen of Python
 print("Flat is better than nested.")
 print("Now is better than never.")
